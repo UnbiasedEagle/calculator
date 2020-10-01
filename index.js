731d1346a2eb5804842bbb42157fd6513b200f10
unknown <imsaurabhsingh007@gmail.com>
2020-10-01 11:50:55 +0530

    Changed substract filename to subtract and changed the function name to subtract to

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const hello = () => {
-    return;
+	return;
 };
 const multiply = (a, b) => {
-    return a * b;
+	return a * b;
 };
 
 const add = (a, b) => {
-    return a + b;
+	return a + b;
 };
 
 const subtract = (a, b) => {
-    return a - b;
+	return a - b;
 };
 const divide = (a, b) => {
-    if (b === 0)
-        return undefined
-    return a / b;
+	if (b === 0) return undefined;
+	return a / b;
 };
 
 hello();
 
 module.exports = {
-    add,
-    multiply,
-    subtract,
-    divide
+	add,
+	multiply,
+	subtract,
+	divide
 };
---
+++ src/methods/subtract.js
@@ -0,0 +1,6 @@
+const subtract = (a, b) => {
+	if (typeof a === 'string' || typeof b === 'string') {
+		return 'Please Provide Numbers';
+	}
+	return a - b;
+};
